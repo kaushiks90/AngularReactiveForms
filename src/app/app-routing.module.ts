9697b6233c97290a23225ce496516c46830d6044
kaushiks90 <kaushiks90@gmail.com>
2020-05-10 12:23:22 +0530

    Video3 setting up routing in a separate routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ListEmployeesComponent } from "./employee/list-employees.component";
+import { CreateEmployeeComponent } from "./employee/create-employee.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "list", component: ListEmployeesComponent },
+  { path: "create", component: CreateEmployeeComponent },
+  { path: "", redirectTo: "/list", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
