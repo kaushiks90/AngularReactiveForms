d0437dac0db734af4ab043fd8508b15d071d6ce7
kaushiks90 <kaushiks90@gmail.com>
2020-05-10 12:56:39 +0530

    Video4 Reactive Forms in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CreateEmployeeComponent } from './employee/create-employee.component';
-import { ListEmployeesComponent } from './employee/list-employees.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CreateEmployeeComponent } from "./employee/create-employee.component";
+import { ListEmployeesComponent } from "./employee/list-employees.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CreateEmployeeComponent,
-    ListEmployeesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CreateEmployeeComponent, ListEmployeesComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
