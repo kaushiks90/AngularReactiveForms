935944b6a6e4da90463c4eb777a8bbd360157190
kaushiks90 <kaushiks90@gmail.com>
2020-05-18 10:31:42 +0530

    Video 26 creating Employee List with Json server

--- src/app/employee/list-employees.component.ts
+++ src/app/employee/list-employees.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { EmployeeService } from "./employee.service";
+import { IEmployee } from "./IEmployee";
 
 @Component({
-  selector: 'app-list-employees',
-  templateUrl: './list-employees.component.html',
-  styleUrls: ['./list-employees.component.css']
+  selector: "app-list-employees",
+  templateUrl: "./list-employees.component.html",
+  styleUrls: ["./list-employees.component.css"],
 })
 export class ListEmployeesComponent implements OnInit {
+  employees: IEmployee[];
 
-  constructor() { }
+  constructor(private _employeeService: EmployeeService) {}
 
   ngOnInit() {
+    this._employeeService.getEmployees().subscribe(
+      (employeeList) => (this.employees = employeeList),
+      (err) => console.log(err)
+    );
   }
-
 }
