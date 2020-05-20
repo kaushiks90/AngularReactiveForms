23a4c8d0082797d634004734d323727cf75d4f9d
kaushiks90 <kaushiks90@gmail.com>
2020-05-20 19:15:35 +0530

    video37 Preloading angular modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home.component";
 import { PageNotFoundComponent } from "./page-not-found.component";
+import { PreloadAllModules } from "@angular/router";
 
 const routes: Routes = [
   // home route
@@ -18,7 +19,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
